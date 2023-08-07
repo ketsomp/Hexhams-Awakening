6432d2fd69b747d9b26ffdf238e6d9a801c5576c
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-08 00:24:11 +0530

    added (temporary) map, tile assets

--- code/level.py
+++ code/level.py
@@ -3,7 +3,8 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
-
+from utility import import_csv_layout,import_folder
+from random import choice
 class Level:
     def __init__(self):
         #get pygame display surface
@@ -16,16 +17,37 @@ class Level:
         self.create_map()
     
     def create_map(self):
-        for row_index,row in enumerate(WORLD_MAP):
-            for col_index,col in enumerate(row):
-                x=col_index*TILESIZE
-                y=row_index*TILESIZE
-                # col - current tile being iterated over
-                # case for each type of data in WORLD_MAP
-                if col=='x':
-                    Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
-                if col=='p':
-                    self.player=Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+        #        # col - current tile being iterated over
+        #        # case for each type of data in WORLD_MAP
+        #        if col=='x':
+        #            Tile((x,y),[self.visi  ble_sprites,self.obstacle_sprites])
+        #        if col=='p':
+        #            self.player=Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+        layout={
+            'boundary':import_csv_layout('../graphics/maps/map_FloorBlocks.csv'),
+            'grass':import_csv_layout('../graphics/maps/map_Grass.csv'),
+            'object':import_csv_layout('../graphics/maps/map_Objects.csv')
+
+        }
+        graphics={
+            'grass':import_folder('../graphics/grass'),
+            'objects': import_folder('../graphics/objects')
+        }
+        for style,layout in layout.items():
+            for row_index,row in enumerate(layout):
+                for col_index,col in enumerate(row):
+                    if col!='-1':
+                        x=col_index*TILESIZE
+                        y=row_index*TILESIZE
+                        if style=='boundary':
+                            Tile((x,y),[self.obstacle_sprites],'invisible')
+                        if style=='grass':
+                            rand_grass_image=choice(graphics['grass'])
+                            Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass',rand_grass_image)
+                        if style=='object':
+                            surf=graphics['objects'][int(col)]
+                            Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
+        self.player=Player((2000,1430),[self.visible_sprites],self.obstacle_sprites)
 
 
     def run(self):
@@ -41,12 +63,21 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_width=self.display_surface.get_size()[0]//2
         self.half_height=self.display_surface.get_size()[1]//2
         self.offset=pygame.math.Vector2(100,200)
+
+        #creating floor
+        self.floor_surface=pygame.image.load('../graphics/map_assets/ground.png')
+        self.floor_rect=self.floor_surface.get_rect(topleft=(0,0))
     
     def custom_draw(self,player):
         #getting offset
         self.offset.x=player.rect.centerx-self.half_width
         self.offset.y=player.rect.centery-self.half_height
+
+        #drawing floor
+        floor_offset_pos=self.floor_rect.topleft-self.offset
+        self.display_surface.blit(self.floor_surface,floor_offset_pos)
+
         #camwera movement
-        for sprite in self.sprites():
+        for sprite in sorted(self.sprites(),key=lambda sprite:sprite.rect.centery):
             offset_pos=sprite.rect.topleft-self.offset
             self.display_surface.blit(sprite.image,offset_pos)
--- code/player.py
+++ code/player.py
@@ -6,6 +6,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image=pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect=self.image.get_rect(topleft=pos)
+        self.hitbox=self.rect.inflate(0,-26) #set hitbox within image instead of rectangle
 
         self.direction=pygame.math.Vector2()
         self.speed=5
@@ -29,30 +30,30 @@ class Player(pygame.sprite.Sprite):
             self.direction.x=0
 
     def move(self,speed):
-        ''' to prevent diagonal speed boost
         if self.direction.magnitude()!=0:
             self.direction=self.direction.normalize()
-        '''
-        self.rect.x+=self.direction.x*speed
+
+        self.hitbox.x+=self.direction.x*speed
         self.collision('h')
-        self.rect.y+=self.direction.y*speed
+        self.hitbox.y+=self.direction.y*speed
         self.collision('v')
+        self.rect.center=self.hitbox.center
 
     def collision(self,direction):
         if direction=='h':
             for sprite in self.obstacle_sprites:
-                if sprite.rect.colliderect(self.rect):
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.x>0: #moving right
-                        self.rect.right=sprite.rect.left
+                        self.hitbox.right=sprite.hitbox.left
                     if self.direction.x<0: #moving left
-                        self.rect.left=sprite.rect.right
+                        self.hitbox.left=sprite.hitbox.right
         if direction=='v':
             for sprite in self.obstacle_sprites:
-                if sprite.rect.colliderect(self.rect):
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.y<0: #moving down
-                        self.rect.top=sprite.rect.bottom
+                        self.hitbox.top=sprite.hitbox.bottom
                     if self.direction.y>0: #moving up
-                        self.rect.bottom=sprite.rect.top
+                        self.hitbox.bottom=sprite.hitbox.top
     
     def update(self):
         self.input()
