a08545a65ec081ccdeab31a5dca398daa9cad940
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-09 15:26:29 +0530

    health bar, energy bar data

--- code/ui.py
+++ code/ui.py
@@ -3,7 +3,13 @@ from settings import *
 
 class UI:
     def __init__(self):
-        pass
+        # general
+        self.display_surface=pygame.display.get_surface()
+        self.font=pygame.font.Font(UI_FONT,UI_FONT_SIZE)
+
+        # bar setup
+        self.health_bar_rect=pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
+        self.energy_bar_rect=pygame.Rect(10,35,ENERGY_BAR_WIDTH,BAR_HEIGHT) # (l,t,b,h)
 
     def display(self,player):
-        pass
+        pygame.draw.rect(self.display_surface,BLACK,self.health_bar_rect)
