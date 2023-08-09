997c28c0db055c869c466f56d85745febf7adb04
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-09 15:15:44 +0530

    added stats variables

--- code/player.py
+++ code/player.py
@@ -17,7 +17,6 @@ class Player(pygame.sprite.Sprite):
 
         #movement
         self.direction=pygame.math.Vector2()
-        self.speed=5
         self.attacking=False
         self.attack_cd=400
         self.attack_duration=None
@@ -30,6 +29,13 @@ class Player(pygame.sprite.Sprite):
         self.can_switch_sweapon=True
         self.weapon_switch_cd=None
         self.switch_duration_cd=200
+
+        #stats
+        self.stats = {'health': 100,'energy':60,'attack': 10,'magic': 4,'speed': 6}
+        self.health=self.stats['health']
+        self.energy=self.stats['energy']
+        self.xp=696
+        self.speed=self.stats['speed']
 
         self.obstacle_sprites=obstacle_sprites
 
