1a8028a2e77ff51ff6ac70e4924a2e9790aad21e
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-09 15:22:36 +0530

    added various parameters

--- code/settings.py
+++ code/settings.py
@@ -5,6 +5,25 @@ FPS      = 60
 TILESIZE = 64
 BLACK = (0, 0, 0)
 
+#ui
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+UI_FONT = '../graphics/font/joystix.ttf'
+UI_FONT_SIZE = 18
+BAR_HEIGHT = 20
+ITEM_BOX_SIZE = 80
+
+# general colors
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+ 
+# ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
 # weapons 
 weapon_data = {
     'sword': {'cooldown': 100, 'damage': 15,'graphic':'../graphics/weapons/sword/full.png'},
