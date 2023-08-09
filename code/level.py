942109c56ea4c617f8069e330759c2375e0de916
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-09 15:20:18 +0530

    display support

--- code/level.py
+++ code/level.py
@@ -71,6 +71,7 @@ class Level:
         #update and draw game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.ui.display(self.player)
         debug(self.player.status)
 
 class YSortCameraGroup(pygame.sprite.Group):
---
+++ code/ui.py
@@ -0,0 +1,9 @@
+import pygame
+from settings import *
+
+class UI:
+    def __init__(self):
+        pass
+
+    def display(self,player):
+        pass
