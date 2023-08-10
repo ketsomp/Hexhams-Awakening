a958ceafcb41d5a2e0b052517db6c8ea051c5cdb
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-10 17:16:57 +0530

    enemy death animations

--- code/enemy.py
+++ code/enemy.py
@@ -4,7 +4,7 @@ from entity import Entity
 from utility import *
 
 class Enemy(Entity):
-    def __init__(self,monster_name,pos,groups,obstacle_sprites,damage_player):
+    def __init__(self,monster_name,pos,groups,obstacle_sprites,damage_player,trigger_death_particles):
         # general
         super().__init__(groups)
         self.sprite_type='enemy'
@@ -32,6 +32,7 @@ class Enemy(Entity):
         self.attack_time=None
         self.attack_cooldown=400
         self.damage_play=damage_player
+        self.trigger_death_particles=trigger_death_particles
 
         # spawn protection
         self.vulnerable=True
@@ -121,6 +122,7 @@ class Enemy(Entity):
     def check_death(self):
         if self.health<=0:
             self.kill()
+            self.trigger_death_particles(self.rect.center,self.monster_name) # monster_name as key of particles dict is monster names
     
     def hit_reaction(self):
         if not self.vulnerable:
--- code/level.py
+++ code/level.py
@@ -80,7 +80,8 @@ class Level:
                                       (x,y),
                                       [self.visible_sprites,self.attackable_sprites],
                                       self.obstacle_sprites,
-                                      self.damage_player)
+                                      self.damage_player,
+                                      self.trigger_death_particles)
 
     def create_attack(self):
         self.current_attack=Weapon(self.player,[self.visible_sprites,self.attack_sprites])
@@ -115,6 +116,9 @@ class Level:
             self.player.hurt_time=pygame.time.get_ticks()
             #self.animation_player.create_particles(attack_type,self.player.rect.center,self.visible_sprites)
             #for animation effect created by enemy attacks (unecessary with current enemy sprites)
+
+    def trigger_death_particles(self,pos,particle_type):
+        self.animation_player.create_particles(particle_type,pos,self.visible_sprites)
 
     def run(self):
         #update and draw game
