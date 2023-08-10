b2a297814e23f56ed4698e2a7c7a76c55ebbb97f
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-10 17:43:41 +0530

    replaced rock with arrow

--- code/settings.py
+++ code/settings.py
@@ -35,7 +35,7 @@ weapon_data = {
 
 # projectiles
 proj_data = {
-    'rock': {'strength': 5,'count': 20,'graphic':'../graphics/projectiles/rock.png'},
+    'arrow': {'strength': 5,'count': 20,'graphic':'../graphics/projectiles/arrow.png'},
     'hp_potion' : {'strength': 20,'count': 10,'graphic':'../graphics/projectiles/hp_potion.png'}}
 
 # enemies
