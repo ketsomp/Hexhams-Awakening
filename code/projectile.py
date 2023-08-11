a4ce07104822a37547258f3b25c6db5b4b688ad8
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-11 20:30:02 +0530

    spikes generate at random on command, no functionality

--- code/projectile.py
+++ code/projectile.py
@@ -1,32 +1,10 @@
 import pygame
 from settings import *
-from level import Level
-
-class Knife(pygame.sprite.Sprite):
-    def __init__(self,player,pos,direction):
-        pygame.sprite.Sprite.__init__(self)
-        self.image=pygame.transform.rotate(pygame.image.load('../graphics/projectiles/knife.png'),-45)
-        self.rect=self.image.get_rect
-        self.rect.x=pos[0]
-        self.rect.y=pos[1]
-        self.direction=player.status.split('_')[0]
-        self.speed=5
-    
-    def update(self):
-        if self.direction=='right':
-            self.rect.x+=self.speed
-        elif self.direction=='left':
-            self.rect.x-=self.speed
-        if self.rect.x>WIDTH or self.rect.x<0:
-            self.kill()
-        killed=pygame.sprite.spritecollide(self,Level.attackable_sprites,True)
-        if killed:
-            self.kill()
+from random import randint
 
 class ItemPlayer:
     def __init__(self,animation_player):
         self.animation_player=animation_player
-
 
     def heal(self,player,strength,cost,groups):
         if player.energy>=cost:
@@ -41,11 +19,11 @@ class ItemPlayer:
     def throw(self,player,cost,groups):
         if player.energy>=cost:
             player.energy-=cost
-            if player.status.split('_')[0]=='right:':
+            if player.status.split('_')[0]=='right':
                 direction=pygame.math.Vector2(1,0)
-            elif player.status.split('_')[0]=='left:':
+            elif player.status.split('_')[0]=='left':
                 direction=pygame.math.Vector2(-1,0)
-            elif player.status.split('_')[0]=='up:':
+            elif player.status.split('_')[0]=='up':
                 direction=pygame.math.Vector2(0,-1)
             else:
                 direction=pygame.math.Vector2(0,1)
@@ -53,7 +31,11 @@ class ItemPlayer:
             for i in range(1,6):
                 if direction.x: 
                     offset_x=(direction.x*i)*TILESIZE
-                    x=player.rect.centerx+offset_x
-                    y=player.rect.centery
-                    self.animation_player.create_particles('knife',(x,y),groups)
-                else: pass
+                    x=player.rect.centerx+offset_x+randint(-TILESIZE//3,TILESIZE//3)
+                    y=player.rect.centery+randint(-TILESIZE//3,TILESIZE//3)
+                    self.animation_player.create_particles('spike',(x,y),groups)
+                else: 
+                    offset_y=(direction.y*i)*TILESIZE
+                    x=player.rect.centerx+randint(-TILESIZE//3,TILESIZE//3)
+                    y=player.rect.centery+offset_y+randint(-TILESIZE//3,TILESIZE//3)                    
+                    self.animation_player.create_particles('spike',(x,y),groups)
