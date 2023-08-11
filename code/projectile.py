c98a26b32383eab019476adec9b3aa4882ac150e
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-11 16:33:11 +0530

    particles when health potion is cast fixed

--- code/projectile.py
+++ code/projectile.py
@@ -1,6 +1,5 @@
 import pygame
 from settings import *
-from particles import AnimationPlayer
 
 class ItemPlayer:
     def __init__(self,animation_player):
@@ -12,8 +11,8 @@ class ItemPlayer:
             player.energy-=cost
             if player.health>=player.stats['health']: #checking if current health exceeds max health
                 player.health=player.stats['health'] # cap at max health
-            self.animation_player.create_particles('aura',player.rect.center,groups)
-            self.animation_player.create_particles('heal',player.rect.center+pygame.math.Vector2(0,-60),groups)
+            self.animation_player.create_particles('aura',player.rect.center,groups) # aura when cast
+            self.animation_player.create_particles('heal',player.rect.center+pygame.math.Vector2(0,-60),groups) # healing thing when cast
 
 
     def shoot(self):
