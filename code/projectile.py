ecbc3f769b19498272881e15716005e8fddba748
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-11 16:32:24 +0530

    healing mechanic using health potion with particles

--- code/projectile.py
+++ code/projectile.py
@@ -1,12 +1,20 @@
 import pygame
 from settings import *
+from particles import AnimationPlayer
 
 class ItemPlayer:
     def __init__(self,animation_player):
         self.animation_player=animation_player
 
-    def heal(self):
-        pass
+    def heal(self,player,strength,cost,groups):
+        if player.energy>=cost:
+            player.health+=strength
+            player.energy-=cost
+            if player.health>=player.stats['health']: #checking if current health exceeds max health
+                player.health=player.stats['health'] # cap at max health
+            self.animation_player.create_particles('aura',player.rect.center,groups)
+            self.animation_player.create_particles('heal',player.rect.center+pygame.math.Vector2(0,-60),groups)
+
 
     def shoot(self):
         pass
