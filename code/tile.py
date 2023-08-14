c39bf7d63ff54c19a9941643ca417d4f24fa9315
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-15 00:14:48 +0530

    removed certain tile hitbox offsets for new objects and map

--- code/tile.py
+++ code/tile.py
@@ -5,11 +5,11 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups,sprite_type,surface=pygame.Surface((TILESIZE,TILESIZE))):
         super().__init__(groups)
         self.sprite_type=sprite_type
-        y_offset=HITBOX_OFFSET[sprite_type]
+        #y_offset=HITBOX_OFFSET[sprite_type]
         self.image=surface.convert_alpha()
         if sprite_type=='object':
             #offset of larger sprites
             self.rect=self.image.get_rect(topleft=(pos[0],pos[1]-TILESIZE))
 
         self.rect=self.image.get_rect(topleft=pos)
-        self.hitbox=self.rect.inflate(0,y_offset) #set hitbox within image instead of rectangle
+        self.hitbox=self.rect.inflate(0,0) #set hitbox within image instead of rectangle
