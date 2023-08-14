6c85f17e6e1da0a4915a189175bb09f0d748d107
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-15 00:12:34 +0530

    fps counter

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ import pygame,sys
 from settings import *
 from level import Level
 
+pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
 
 class Game:
@@ -10,6 +11,7 @@ class Game:
         pygame.display.set_caption("Hexham's Awakening")
         pygame.display.set_icon(pygame.image.load('../graphics/logo/ha_logo.png').convert_alpha())
         self.clock = pygame.time.Clock()
+        self.font=pygame.font.Font(UI_FONT,20)
 
         self.level=Level()
 
@@ -17,9 +19,13 @@ class Game:
         main_ost=pygame.mixer.Sound('../audio/ost.ogg')
         main_ost.set_volume(0.5)
         main_ost.play(loops=-1)
+
+    def render(self):
+        self.text=self.font.render(str(round(self.clock.get_fps())),True,(255,255,255))
+        self.screen.blit(self.text,(WIDTH-50,0))  
     
     def run(self):
-        #game loop
+        #game loop 
         while True:
             #keys pressed
             for event in pygame.event.get():
@@ -34,10 +40,12 @@ class Game:
             #graphics drawn
             self.screen.fill(WATER_COLOR)
             self.level.run()
+            self.render()
 
             #update display
             pygame.display.update()
             self.clock.tick(FPS)
+
 
 if __name__== '__main__':
     game = Game()
