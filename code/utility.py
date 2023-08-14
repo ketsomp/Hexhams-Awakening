b5f650d24e921dd9586aef705504093d823189c2
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-15 00:15:53 +0530

    edited to sort triple digit numbered files properly

--- code/utility.py
+++ code/utility.py
@@ -10,18 +10,23 @@ def import_csv_layout(path):
         for row in layout:
             terrain_map.append(list(row))
         return terrain_map
-
+    
 def import_folder(path):
     surface_list = []
 
     for a,b,img_files in walk(path):
-        for image in sorted(img_files):
+        if 'object' in path:
+            img_files.remove( '.DS_Store') # .DS_Store is the bane of my existence
+
+            print(img_files)
+            img_files.sort(key=lambda id:int(id.split('.')[0]))
+        else:
+            img_files.sort()
+        for image in img_files:
+            print(image) if 'object' in path else 6+9
             full_path = path + '/' + image
             if not image.startswith('.DS'): # to prevent those blasted .DS_Store files from being parsed
                 image_surf = pygame.image.load(full_path).convert_alpha()
                 surface_list.append(image_surf)
 
     return surface_list
-
-#print(import_csv_layout('../graphics/maps/map_FloorBlocks.csv'))
-
