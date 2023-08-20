0fa8da367e8ae24fa2c5b5ebc296b9f7c6d38d0e
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-20 18:11:27 +0530

    attack delay rework

--- code/settings.py
+++ code/settings.py
@@ -55,7 +55,7 @@ proj_data = {
 
 # enemies
 monster_data = {
-    'skeleton': {'health': 300,'xp':250,'damage':40,'attack_type': 'slash', 'attack_sound':'../audio/attack/claw.wav', 'speed': 2, 'resistance': 4, 'attack_radius': 120, 'aggro_radius': 400,'hit_cd':4000},
-    'mushroom': {'health': 100,'xp':100,'damage':20,'attack_type': 'slash',  'attack_sound':'../audio/attack/claw.wav','speed': 2, 'resistance': 3, 'attack_radius': 80, 'aggro_radius': 360,'hit_cd':400},
-    'goblin': {'health': 100,'xp':110,'damage':8,'attack_type': 'claw', 'attack_sound':'../audio/attack/claw.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 60, 'aggro_radius': 350,'hit_cd':300},
-    'flying_eye': {'health': 70,'xp':120,'damage':6,'attack_type': 'thunder', 'attack_sound':'../audio/attack/slash.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 50, 'aggro_radius': 300,'hit_cd':200}}
+    'skeleton': {'health': 300,'xp':250,'damage':40,'attack_type': 'slash', 'attack_sound':'../audio/attack/claw.wav', 'speed': 2, 'resistance': 4, 'attack_radius': 120, 'aggro_radius': 400,'atk_delay':10},
+    'mushroom': {'health': 100,'xp':100,'damage':20,'attack_type': 'slash',  'attack_sound':'../audio/attack/claw.wav','speed': 2, 'resistance': 3, 'attack_radius': 80, 'aggro_radius': 360,'atk_delay':400},
+    'goblin': {'health': 100,'xp':110,'damage':8,'attack_type': 'claw', 'attack_sound':'../audio/attack/claw.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 60, 'aggro_radius': 350,'atk_delay':300},
+    'flying_eye': {'health': 70,'xp':120,'damage':6,'attack_type': 'thunder', 'attack_sound':'../audio/attack/slash.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 50, 'aggro_radius': 300,'atk_delay':200}}
