aaaa3e09b424c823c78900ca560a2875b939c554
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-22 22:43:50 +0530

    buttons

---
+++ code/button.py
@@ -0,0 +1,26 @@
+import pygame
+
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.clicked = False
+
+    def draw(self,screen):
+        action = False
+        # get cursor position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and click conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                action = True
+                self.clicked = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        # draw button
+        screen.blit(self.image, self.rect)
+        return action
