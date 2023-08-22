f6905daff9a8406fcf40ac9919008f7fc434316c
Aniket <66839991+deathskull728@users.noreply.github.com>
2023-08-22 22:41:36 +0530

    add_outline_to_image function for better text

--- code/utility.py
+++ code/utility.py
@@ -27,3 +27,19 @@ def import_folder(path):
                 surface_list.append(image_surf)
 
     return surface_list
+
+def add_outline_to_image(image: pygame.Surface, thickness: int, color: tuple, color_key: tuple = (255, 0, 255)) -> pygame.Surface:
+    mask = pygame.mask.from_surface(image)
+    mask_surf = mask.to_surface(setcolor=color)
+    mask_surf.set_colorkey((0, 0, 0))
+
+    new_img = pygame.Surface((image.get_width() + 2, image.get_height() + 2))
+    new_img.fill(color_key)
+    new_img.set_colorkey(color_key)
+
+    for i in -thickness, thickness:
+        new_img.blit(mask_surf, (i + thickness, thickness))
+        new_img.blit(mask_surf, (thickness, i + thickness))
+    new_img.blit(image, (thickness, thickness))
+
+    return new_img
